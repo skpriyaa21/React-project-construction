d933d39b11b1b403844f1f580107acf7ce91347f
skpriyaa21 <krishnapriyasubramanian2103@gmail.com>
2025-11-11 15:36:26 +0530

    Fix:Filename

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import HeroSection from "./components/Herosection";
+import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import ServicesSection from "./components/ServicesSection";
 import ProjectsSection from "./components/ProjectsSection";
